c5a459dc4e1f4431542ea5f91947de4777ef26e3
ethan <0114017@student.isf.edu.hk>
2025-06-27 01:57:24 +0800

    Database setup

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,44 @@
 
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
+ending = False
+df = pd.read_csv("./dataframe/order.csv")
+print(df)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
+@app.route('/end')
+def signal_end():
+    ending = True
+    return "ended"
+
+def main_loop():
+    #threshold
+
+    while (not ending):
+        
+        # fetch from Binance
+
+
+        # write to pandas w/ csv
+
+        # make your decision
+
+
+
+        sleep(30)
+        break
+
+
+
 if __name__ == '__main__':
+
+    main_loop()
+
+    # Start our API
     app.run(debug=True)
